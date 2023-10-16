7e618d1e71863bfb4b83f8ae7df8da3ae6c53e0f
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-10-16 15:20:30 -0400

    add search functionality

--- src/app/countries/page.tsx
+++ src/app/countries/page.tsx
@@ -1,8 +1,8 @@
 /* eslint-disable @next/next/no-img-element */
 // src/app/countries/page.tsx
-'use client'
+'use client';
 import React, { useEffect, useState } from 'react';
-import Loading from '../../components/Loading';  // Adjust the path as necessary
+import Loading from '../../components/Loading'; // Adjust the path as necessary
 
 interface Language {
   code: string;
@@ -30,28 +30,40 @@ interface Country {
 const CountriesPage = () => {
   const [countries, setCountries] = useState<Country[]>([]);
   const [loading, setLoading] = useState(true);
+  const [searchTerm, setSearchTerm] = useState('');
 
   useEffect(() => {
     fetch('/api/countries')
-      .then(response => response.json())
-      .then(data => {
+      .then((response) => response.json())
+      .then((data) => {
         setCountries(data);
         setLoading(false);
       });
   }, []);
 
+  const filteredCountries = countries.filter((country) =>
+    country.name.toLowerCase().includes(searchTerm.toLowerCase()),
+  );
+
   return (
     <div className="min-h-screen" data-theme="business">
       <h1 className="text-center py-8 text-4xl">Countries</h1>
+      <input
+        type="text"
+        placeholder="Search countries..."
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+        className="w-1/2 block mx-auto px-4 py-2 mb-6 border rounded"
+      />
       {loading ? (
         <Loading text="countries" />
       ) : (
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 p-6">
-          {countries.map((country: Country, index: number) => (
+          {filteredCountries.map((country: Country, index: number) => (
             <div
               key={index}
               className="p-6 rounded-lg shadow-lg"
-              data-theme="luxury"
+              data-theme="corporate"
             >
               <h2 className="mb-4 font-sans text-3xl text-center font-bold">
                 {country.name}
