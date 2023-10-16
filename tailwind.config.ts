b60506187e31f6fc0962012a1f7ed403bfe5370d
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-10-16 07:40:53 -0400

    install and configure daisy ui

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+   daisyui: {
+    themes: true,
+   },
   content: [
     './app/**/*.{js,ts,jsx,tsx,mdx}',
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
@@ -9,7 +12,25 @@ module.exports = {
     './src/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        overlay: '#f4fff8ff', // Mint Cream
+        background: '#000f08ff', // Night
+        button: '#F8F32B', // Cadet Gray
+        headline: '#f4fff8ff', // Mint Cream
+        paragraph: '#8baaadff', // Cadet Gray
+        buttonText: '#000f08ff', // Night
+        stroke: '#4d4847ff', // Davy's Gray
+        main: '#8baaadff', // Cadet Gray
+        highlight: '#1c3738ff', // Gunmetal
+        secondary: '#f4fff8ff', // Mint Cream
+        tertiary: '#4d4847ff', // Davy's Gray
+      },
+    },
+    fontFamily: {
+      sans: ['Poppins', 'sans'],
+      serif: ['Roboto Slab', 'serif'],
+    },
   },
-  plugins: [],
+  plugins: [require('daisyui')],
 };
