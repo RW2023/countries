82da7df92710659d866c6be2f904b3bdb018b7c6
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-10-16 15:40:39 -0400

    update home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,36 @@
-// import Image from 'next/image'
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex justify-center align-middle flex-col text-center h-screen text-4xl">
-      <h1>Home</h1>
-      <p>This is the Home Page </p>
+    <div className="hero-overlay bg-opacity-80">
+      <div className="relative hero min-h-screen">
+        <video
+          autoPlay
+          loop
+          muted
+          playsInline
+          className="absolute top-0 left-0 object-cover w-full h-full"
+          src="/earth2.mp4"
+        ></video>
+        <div className="relative hero-content text-center text-neutral-content">
+          <div className="max-w-md">
+            <h1 className="mb-5 text-5xl font-bold text-headline text-stroke-black">
+             Countries
+            </h1>
+            <p className="mb-5 text-2xl text-headline">
+                Discover the world and it&rsquo;s countries
+            </p>
+            <Link href="/countries">
+              <button
+                type="button"
+                className="btn btn-primary bg-button text-black hover:text-headline hover:bg-background"
+              >
+                Search Countries
+              </button>
+            </Link>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
