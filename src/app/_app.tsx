cbe8ef5b08ab25ece6d98cf73043ad522b8cd30d
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-10-16 09:10:41 -0400

    create app.tsx and import global styles

---
+++ src/app/_app.tsx
@@ -0,0 +1,9 @@
+// pages/_app.js or app/pages/_app.js
+import './globals.css';
+import type { AppProps } from 'next/app';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
