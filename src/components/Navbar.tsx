f2620b1a89e6d3b2801ea0297b6c37f00f5fc2ac
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-10-17 02:56:57 -0400

    make navbar responsive on mobile

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,22 +3,21 @@ import Link from 'next/link';
 
 function Navbar() {
   return (
-    <div className="navbar bg-base-100 flex justify-between items-center p-4">
-      <div>
-        <Link href="/" className="btn btn-ghost normal-case text-xl mr-4">
+    <div className="navbar bg-base-100 flex flex-col md:flex-row justify-between items-center p-4">
+      <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-4">
+        <Link href="/" className="btn btn-ghost normal-case text-xl">
           Home
         </Link>
-        <Link
-          href="/countries"
-          className="btn btn-ghost normal-case text-xl mr-4"
-        >
+        <Link href="/countries" className="btn btn-ghost normal-case text-xl">
           Countries
         </Link>
-        <Link href="/about" className="btn btn-ghost normal-case text-xl mr-4">
+        <Link href="/about" className="btn btn-ghost normal-case text-xl">
           About
         </Link>
       </div>
-      <span className="btn btn-ghost normal-case text-2xl">Countries API</span>
+      <span className="btn btn-ghost normal-case text-2xl mt-4 md:mt-0">
+        Countries API
+      </span>
     </div>
   );
 }
