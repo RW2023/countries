67954e326cc4dd128129020da27a86869ddec3b8
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-12-30 12:45:18 -0500

    Refactor layout component and add font-awesome stylesheet

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,24 +1,29 @@
-
-import Navbar from '@/components/Navbar'
-import './globals.css'
-import type { Metadata } from 'next'
+import Navbar from '@/components/Navbar';
+import './globals.css';
+import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
   title: 'Countries API',
   description: 'Explore the world with countries API',
-}
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en" data-theme="dracula">
+      <head>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
+        />
+      </head>
       <body>
-<Navbar />
+        <Navbar />
         {children}
-        </body>
+      </body>
     </html>
-  )
+  );
 }
