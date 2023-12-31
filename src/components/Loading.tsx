483099b0ba8189201147358404d4bad8f5d7f45f
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-12-30 20:49:12 -0500

    Refactor Loading component to use a div container. center with flex

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -2,10 +2,10 @@
 import React from 'react';
 
 const Loading = ({ text }: { text: string }) => (
-  <>
+  <div className='flex flex-col justify-center items-center min-h-full'>
     <p className="text-2xl">Loading {text}...🚀</p>
     <span className="loading loading-bars loading-lg"></span>
-  </>
+  </div>
 );
 
 export default Loading;
