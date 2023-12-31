b939b4cf80f31d483da2d560939918ef9a9e0f73
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-12-30 21:28:38 -0500

    Refactor animation in _app.tsx to fix error with deprecation waring

--- src/app/_app.tsx
+++ src/app/_app.tsx
@@ -1,25 +1,20 @@
 import { AnimatePresence, motion } from 'framer-motion';
 import './globals.css';
 import type { AppProps } from 'next/app';
-import Layout from './layout';
+import Layout from './layout'; 
 
 function MyApp({ Component, pageProps, router }: AppProps) {
   return (
     <Layout>
       <AnimatePresence mode="wait">
-        <motion.div
-          key={router.route}
-          initial="pageInitial"
-          animate="pageAnimate"
-          variants={{
-            pageInitial: {
-              opacity: 0,
-            },
-            pageAnimate: {
-              opacity: 1,
-            },
-          }}
-        >
+        <motion.div key={router.route} initial="pageInitial" animate="pageAnimate" variants={{
+          pageInitial: {
+            opacity: 0
+          },
+          pageAnimate: {
+            opacity: 1
+          },
+        }}>
           <Component {...pageProps} />
         </motion.div>
       </AnimatePresence>
