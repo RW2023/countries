47f6295a2edfed9039d87b34c2ae5e1774a95d62
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-12-30 22:12:16 -0500

    Add favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,12 +3,13 @@ import Document, { Html, Head, Main, NextScript } from 'next/document';
 class MyDocument extends Document {
   render() {
     return (
-      <Html lang='en'>
+      <Html lang="en">
         <Head>
           <link
             href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&family=Roboto+Slab:wght@400;700&display=swap"
             rel="stylesheet"
           />
+          <link rel="icon" href="/favicon_io/favicon.ico" />
         </Head>
         <body>
           <Main />
